a18d76a8f8578eb055168593749e21ed17c3dedd
durgedancing <durgedancing@gmail.com>
2021-03-19 20:22:39 +0300

    попавила баги

--- games/brain-gcd-game.js
+++ games/brain-gcd-game.js
@@ -10,14 +10,22 @@ const findMaxValue = (n1, n2) => {
 }
 
 const getGcd = (n1, n2) => {
+
     let min = findMinValue(n1, n2);
     let max = findMaxValue(n1, n2);
 
-    if (max % min === 0 ) {
+    if (min === 0) {
+        return max;
+    } else if (max === 0) {
+        return Math.abs(min);
+    }
+
+    if (max % min === 0) {
         return min;
     } else {
-       let newMin = min % (max % min);
-       return getGcd(min, newMin);
+       let newN1 = max % min;
+       let newN2 = min % newN1;
+       return getGcd(newN1, newN2);
     }
 }
 
@@ -25,7 +33,7 @@ export const gcd = () => {
 
     console.log('Find the greatest common divisor of given numbers.');
 
-    for (let i = 0; i < 2;) {
+    for (let i = 0; i < 3;) {
          let number1 = getRandomInt(100);
          let number2 = getRandomInt(100);
 
@@ -40,7 +48,7 @@ export const gcd = () => {
         } else if (answer !== rightAnswer) {
             gameOver(answer, rightAnswer);
             i += 3;
-        } else if (i === 2) {
+        } else if (answer === rightAnswer && i === 2) {
             congratulation();
             i += 1;
         }
